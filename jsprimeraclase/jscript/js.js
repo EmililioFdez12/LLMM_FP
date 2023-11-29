57a6a9564c105f538237d2ea3e012dae5b8e414e
EmililioFdez12 <emiliofdezglld12@gmail.com>
2023-11-29 14:42:28 +0100

    Primeros Pasos

--- jsprimeraclase/jscript/js.js
+++ jsprimeraclase/jscript/js.js
@@ -1,5 +1,31 @@
 
-alert("Hola Mundo!!!");
+// alert("Hola Mundo!!!");
+
+/* let num1 = 1;
+let num2 = 2;
+let num3 = 'cero "es cero"'; */
+
+/* let num1 = 2, num2 = 5, num3 = 12 */
+
+/* alert(typeof num1);
+alert(num2);
+alert(num3); */
+
+let edad = Number(prompt("Escriba su edad"));
+
+/*let gusta = confirm("Te gusta la pagina?");*/
+
+alert(typeof edad); /*Con el prompt lo lee como String hay que pasarlo a numero antes*/
+alert(edad[0]);
+alert(edad);
+
+
+if(edad > 25){
+    alert("Eres muy mayor");
+} else {
+    alert("Eres muy joven");
+}
+
 
 
 
