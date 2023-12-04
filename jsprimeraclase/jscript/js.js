5677b036f9047b8eca57634460144545e7c3f84d
EmililioFdez12 <emiliofdezglld12@gmail.com>
2023-12-04 14:29:08 +0100

    Recuperacion WebPersonal

--- jsprimeraclase/jscript/js.js
+++ jsprimeraclase/jscript/js.js
@@ -11,22 +11,46 @@ let num3 = 'cero "es cero"'; */
 alert(num2);
 alert(num3); */
 
-let edad = Number(prompt("Escriba su edad"));
+/*let edad = Number(prompt("Escriba su edad"));*/
 
 /*let gusta = confirm("Te gusta la pagina?");*/
 
-alert(typeof edad); /*Con el prompt lo lee como String hay que pasarlo a numero antes*/
+/* alert(typeof edad); /*Con el prompt lo lee como String hay que pasarlo a numero antes
 alert(edad[0]);
-alert(edad);
+alert(edad); */
 
-
+/*
 if(edad > 25){
     alert("Eres muy mayor");
 } else {
     alert("Eres muy joven");
 }
+*/
+
+/* for (let i = 0; i <= 5; i++){
+    valores[i] = prompt("Introduzca el valor");
+} */
+
+/*Array*/
+let valores = [];
+
+for (let i = 0; i <= 4; i++){
+    valores.push(prompt("Introduzca el valor"));
+}
 
 
+let maximo = valores[0];
+let suma = 0;
+
+for(let i = 0; i <= 4;i++){
+    if(maximo < valores[i])
+        maximo = valores[i];
+    suma += valores[i];
+}
+
+alert(valores);
+alert("El numero " + maximo + " es el mayor")
+alert(suma / 5);
 
 
 /*let i = 0;
