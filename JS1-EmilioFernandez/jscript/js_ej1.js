f0c4cb92eb1342d3718a54e0b633dcda2e3bddf9
EmililioFdez12 <emiliofdezglld12@gmail.com>
2023-12-15 13:36:15 +0100

    Donete

--- JS1-EmilioFernandez/jscript/js_ej1.js
+++ JS1-EmilioFernandez/jscript/js_ej1.js
@@ -1,39 +1,40 @@
-/*Ejercicio 1*/
-/* 
-alert("1 Escribe un script que muestre tu nombre y el curso y el campo de la informática donde te gustaría trabajar");
+/* Ejercicio 1 */
+alert("EJERCICIO 1");
 let nombre = "Emilio";
 let curso = "1º DAM";
-alert("Nombre: Emilio \nCurso: 1º DAM \nNo me puedo decidir en algo realmente, " 
-+ "pero lo que mas me gustaria sería: Páginas Web(Interfaces), Desarrollo de Aplicaciones o Desarrollo de videojuegos")
- */
+alert("Nombre: " + nombre + "\nCurso: " + curso + "\nNo me puedo decidir en algo realmente, "
+    + "pero lo que más me gustaría sería: Páginas Web(Interfaces), Desarrollo de Aplicaciones o Desarrollo de videojuegos");
 
-
-
-/*2. Realiza la suma de los números que se introduzcan por teclado hasta que se lea un cero. */
-/* let numero;
-let suma = 0;
-
+/* Ejercicio 2 */
+alert("EJERCICIO 2");
+let sumaEj2 = 0;
 do {
-    numero = Number(prompt("Introduzca numeros para sumarlos, (0 para termianr el programa)"));
-    suma += numero;
-} while (numero != 0) {
-    alert("La suma de los numeros dados es = " + suma);
-} */
+    numero = Number(prompt("Introduzca números para sumarlos, (0 para terminar el programa)"));
+    if (isNaN(numero)) {
+        alert("Tienes que introducir números");
+    } else {
+        sumaEj2 += numero;
+    }
+} while (numero !== 0);
+alert("La suma de los números dados es = " + sumaEj2);
 
-/*3. Leer 4 valores numéricos de una cifra. Esos valores en realidad son un solo número y habrá que escribirlo 
-por pantalla completo. Por ejemplo, introduzco el 1, 2, 3 y 4. El resultado es 1234. */
-
-/* let numeroGrande = '';
-
+/* Ejercicio 3 */
+alert("EJERCICIO 3");
+let numeroGrande = '';
 for (let i = 1; i <= 4; i++) {
-    numero = prompt("Introduce el numero " + i);
-    numeroGrande += numero
+    do {
+        numero = prompt("Introduce el número " + i);
+        if (isNaN(numero)) {
+            alert("Introduce números");
+        }
+    } while (isNaN(numero));
+    numeroGrande += numero;
 }
-alert("El resultado es " + numeroGrande); */
+alert("El resultado es " + numeroGrande);
 
-/*4. Leer el valor de la nota de un alumno y poner la nota correspondiente en texto. Por ejemplo,
+/* /*4. Leer el valor de la nota de un alumno y poner la nota correspondiente en texto. Por ejemplo,
 un 7.2 es notable.*/
-
+alert("EJERCICIO 4")
 let nota = Number(prompt("Introduce la nota: "))
 
 if (nota < 5) {
@@ -41,74 +42,81 @@ if (nota < 5) {
 } else if (nota >= 5 && nota <= 7) {
     alert("Un " + nota + " es un bien")
 } else if (nota > 7 && nota < 9) {
-    alert("Un " + nota + " es un notable")  
-}else if (nota >= 9 && nota <= 10) {
+    alert("Un " + nota + " es un notable")
+} else if (nota >= 9 && nota <= 10) {
     alert("Un " + nota + " es un sobresaliente")
 }
 
+/*5. Introducir 10 valores en un array, mostrar la suma de sus valores, el mayor valor, el menor
+valor y la media de los valores.*/
+alert("EJERCICIO 5")
+// 1. Crear un array para almacenar los valores
+let valoresArray = [];
 
+// 2. Utilizar un bucle para introducir 10 valores en el array
+for (let i = 0; i < 10; i++) {
+    // Puedes utilizar prompt para pedir al usuario que ingrese valores
+    let numeroArray = parseInt(prompt("Introduce un valor:"));
 
-/* let num1 = 1;
-let num2 = 2;
-let num3 = 'cero "es cero"'; */
-
-/* let num1 = 2, num2 = 5, num3 = 12 */
-
-/* alert(typeof num1);
-alert(num2);
-alert(num3); */
-
-/*let edad = Number(prompt("Escriba su edad"));*/
-
-/*let gusta = confirm("Te gusta la pagina?");*/
-
-/* alert(typeof edad); /*Con el prompt lo lee como String hay que pasarlo a numero antes
-alert(edad[0]);
-alert(edad); */
-
-/*
-if(edad > 25){
-    alert("Eres muy mayor");
-} else {
-    alert("Eres muy joven");
+    // Agregar el valor al array
+    valoresArray.push(numeroArray);
 }
-*/
 
-/* for (let i = 0; i <= 5; i++){
-    valores[i] = prompt("Introduzca el valor");
-} */
-
-/*Array*/
-let valores = [];
-
-/* for (let i = 0; i <= 4; i++){
-    valores.push(prompt("Introduzca el valor"));
+// 3. Calcular la suma de los valores
+let sumar = 0;
+for (let i = 0; i < valoresArray.length; i++) {
+    sumar += valoresArray[i];
 }
- */
-/*
-let maximo = valores[0];
-let suma = 0;
 
-for(let i = 0; i <= 4;i++){
-    if(maximo < valores[i])
-        maximo = valores[i];
-    suma += valores[i];
-} */
+// 4. Calcular el mayor y menor valor
+let maximo = Math.max(...valoresArray);
+let minimo = Math.min(...valoresArray);
 
-/* alert(valores);
-alert("El numero " + maximo + " es el mayor")
-alert(suma / 5); */
+// 5. Calcular la media de los valores
+let media = suma / valoresArray.length;
+
+// 6. Mostrar los resultados
+alert("Suma de los valores: " + suma);
+alert("Mayor valor: " + maximo);
+alert("Menor valor: " + minimo);
+alert("Media de los valores: " + media);
 
 
-/*let i = 0;
+/*6. Rellenar un array con valores aleatorios mediante una función matemática de 20 elementos
+tipo entero entre el 10 y el 20. El programa tendrá que mostrar el total de valores mayores que
+15, menores que 15 e iguales a 15.*/
+alert("EJERCICIO 6")
+let valoresArray2 = [];
 
-let caracter = "Hola Mundo";
+// 1. Función para obtener un valor aleatorio entre 10 y 20
+function obtenerValorAleatorio() {
+    return Math.floor(Math.random() * (20 - 10 + 1)) + 10;
+}
 
-let vari = false;
-*/
+// 2. Rellenar el array con 20 valores aleatorios
+for (let i = 0; i < 20; i++) {
+    valoresArray2.push(obtenerValorAleatorio());
+}
 
-//const constante
+// 3. Mostrar el array
+alert("Array generado:" + valoresArray2);
 
-// let variable "local"
+// 4. Contar valores mayores, menores e iguales a 15
+let mayores15 = 0;
+let menores15 = 0;
+let iguales15 = 0;
 
-//var variable "global" 
+for (let i = 0; i < valoresArray2.length; i++) {
+    if (valoresArray2[i] > 15) {
+        mayores15++;
+    } else if (valoresArray2[i] < 15) {
+        menores15++;
+    } else {
+        iguales15++;
+    }
+}
+
+// 5. Mostrar resultados
+alert("Total de valores mayores que 15: " + mayores15);
+alert("Total de valores menores que 15: " + menores15);
+alert("Total de valores iguales a 15: " + iguales15);
