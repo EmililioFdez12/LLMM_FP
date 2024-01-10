964e122b98ab859e8997613b9f7f607a7c068b92
EmililioFdez12 <emiliofdezglld12@gmail.com>
2024-01-10 14:41:42 +0100

    Ejercicio 2

---
+++ JS2-EmilioFernandez/jscript/js_ej2.js
@@ -0,0 +1,39 @@
+/* Ejercicio 1 */
+/*let numeracion = {
+    1: "primero",
+    2: "segundo",
+    3: "tercero",
+    4: "cuarto",
+    5: "quinto",
+    6: "sexto",
+    7: "septimo",
+    8: "octavo",
+    9: "noveno",
+    10: "decimo"
+}
+
+let numeroAIntroducir;
+
+do {
+    numeroAIntroducir = prompt("Introduce un número ordinal del 1 al 10:"));
+
+    /*Si es un numero se ejecuta el bucle, si no salta una advertencia */
+    /*if (Number(numeroAIntroducir)) {
+        for (let i in numeracion) {
+            if (numeroAIntroducir == i) {
+                alert(numeracion[i]);
+            }
+        }
+    } else {
+        alert("Debes introducir un número")
+    }
+} while (numeroAIntroducir >= 1 && numeroAIntroducir <= 10);
+*/
+
+/*Ejercicio 2 */
+let numDecimal1 = prompt("Introduce un numero decimal");
+let numDecimal2 = prompt("Introduce un numero decimal");
+
+
+
+/*Ejercicio 3 */
