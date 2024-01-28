33e1709fa584436e7ad721ecd8735299519c22bf
EmilioFdez12 <emiliofdezglld12@gmail.com>
2024-01-28 23:31:18 +0100

    Ej1 relacion 5 done

--- JS5-EmilioFernandez/js/js.js
+++ JS5-EmilioFernandez/js/js.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     // Escucha el evento de clic en el enlace
     // anchor en <a></a>
@@ -21,104 +20,25 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 
-    const IMAGENES = [
-        'images/f40.jpg',
-        'images/toyota_mr2.jpg',
-        'images/porsche911.jpg',
-        'images/audi.jpg',
-        'images/gti.jpg',
-        'images/lambo.jpg',
-        'images/civic.jpg'
-    ];
-
-    document.addEventListener('DOMcontentLoaded',()=>{
-      const inner = document.querySelector('.galeria-inner');
-      const prev = document.querySelector('.retroceder');
-      const next = document.querySelector('.avanzar');
-      const imgWidth = document.querySelector('img').clientWidth;
-
+    document.addEventListener('DOMContentLoaded', () => {
+      const inner = document.querySelector('.carousel-inner');
+      const prev = document.querySelector('.carousel-prev');
+      const next = document.querySelector('.carousel-next');
+      const imgWidth = document.querySelector('.carousel-inner img').clientWidth;
+    
       let currentIndex = 0;
-
-      prev.addEventListener('click',(event)=>{
-        event.preventDefault();
-        currentIndex = Math.max(currentIndex - 1 , 0);
-
-        inner.style.transform = `translateX(-${currentIndex * imgWidth}px)`
-      })
-
-      next.addEventListener('click',(event)=>{
-        event.preventDefault();
-        currentIndex = Math.min(currentIndex + 1 , inner.childElementCount - 1);
-
-        inner.style.transform = `translateX(-${currentIndex * imgWidth}px)`
-      })
-
-    })
-
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /* let indiceImagenActual = 0;
-
-    function avanzarImagen() {
-      indiceImagenActual++;
-      if (indiceImagenActual >= IMAGENES.length) {
-        indiceImagenActual = 0;
-      }
-      inner.style.transform = `translateX(-${indiceImagenActual * imgWidth}px)`;
-      document.getElementById("fotos").src = IMAGENES[indiceImagenActual];
-    }
-
-    function retrocederImagen() {
-      indiceImagenActual--;
-      if (indiceImagenActual < 0) {
-        indiceImagenActual = IMAGENES.length - 1;
-      }
-
-      if (indiceImagenActual >= IMAGENES.length) {
-        indiceImagenActual = 0;
-      }
-      document.getElementById("fotos").src = IMAGENES[indiceImagenActual];
-    }
+      prev.addEventListener('click', (event) => {
+        event.preventDefault();
+        currentIndex = Math.max(currentIndex - 1, 0);
+        inner.style.transform = `translateX(-${currentIndex * imgWidth}px)`;
+      });
     
-    document.getElementById("avanzar").addEventListener("click", avanzarImagen);
-    document.getElementById("retroceder").addEventListener("click", retrocederImagen); */
+      next.addEventListener('click', (event) => {
+        event.preventDefault();
+        currentIndex = (currentIndex + 1) % inner.childElementCount;
+        inner.style.transform = `translateX(-${currentIndex * imgWidth}px)`;
+      });
+    });
+    
+    
